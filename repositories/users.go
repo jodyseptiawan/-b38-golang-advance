c3de244ef1c271d70a073fbde0a8481179c44d94
Jody-septiawan <jody.septiawan5@gmail.com>
2022-09-08 13:24:59 +0700

    Has One

--- repositories/users.go
+++ repositories/users.go
@@ -21,13 +21,15 @@ func RepositoryUser(db *gorm.DB) *repository {
 func (r *repository) FindUsers() ([]models.User, error) {
 	var users []models.User
 	// Using Preload("profile") to find data with relation to profile here ...
-
+	err := r.db.Preload("Profile").Find(&users).Error
+	
 	return users, err
 }
 
 func (r *repository) GetUser(ID int) (models.User, error) {
 	var user models.User
 	// Using Preload("profile") to find data with relation to profile here ...
+	err := r.db.Preload("Profile").First(&user, ID).Error
 
 	return user, err
 }
